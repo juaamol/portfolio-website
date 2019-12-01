e5108c2c47287eafc4ab71aa152bfc3c48d2bf79
juaamol <jambulud@gmail.com>
2019-12-01 15:58:52 +0100

    created components component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AngularMaterialModule } from './modules/material/angular-material/angular-material.module';
+import { Routes, RouterModule } from '@angular/router';
+import { CustomComponentsComponent } from './components/custom-components/custom-components.component';
+import { OpenGraphCardComponent } from './components/open-graph-card/open-graph-card.component';
+
+const appRoutes: Routes = [
+  { path: 'custom-components', component: CustomComponentsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomComponentsComponent,
+    OpenGraphCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    RouterModule.forRoot(appRoutes,{ enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/open-graph-card/open-graph-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-open-graph-card',
+  templateUrl: './open-graph-card.component.html',
+  styleUrls: ['./open-graph-card.component.scss']
+})
+export class OpenGraphCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
