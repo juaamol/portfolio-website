f037146a436f8eeac4fb51dd16fc7d303c8e3c9c
juaamol <jambulud@gmail.com>
2023-02-16 19:02:11 +0100

    Add sorting to skills

--- src/app/components/curriculum/skills-details/skills-details.component.ts
+++ src/app/components/curriculum/skills-details/skills-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface SkillDetails {
   skill: string;
@@ -103,5 +106,26 @@ const SKILL_DATA: SkillDetails[] = [
 })
 export class SkillsDetailsComponent {
   displayedColumns: string[] = ['skill', 'description'];
-  dataSource = SKILL_DATA;
+  dataSource = new MatTableDataSource(SKILL_DATA);
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
+
+  @ViewChild(MatSort) sort: MatSort = {} as MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
