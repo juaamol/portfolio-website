d3ecdefda1921db719971a260a31b5978544b6d2
juaamol <jambulud@gmail.com>
2023-02-16 18:54:24 +0100

    Update missing data

--- src/app/components/curriculum/skills-details/skills-details.component.ts
+++ src/app/components/curriculum/skills-details/skills-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 export interface SkillDetails {
   skill: string;
@@ -7,92 +7,101 @@ export interface SkillDetails {
 
 const SKILL_DATA: SkillDetails[] = [
   {
-    skill: "HTML, HTML5",
+    skill: 'HTML, HTML5',
     description: `Knowledge about HTML.</br>
       <strong>Tags</strong>: semantic and non-semantic tags<br/>
       <strong>Accesibility</strong>: aria attributes, accesibility configurations (breadcrumbs, colors, skip to content, ...), accesible tables, ...
-      `
+      `,
   },
   {
-    skill: "CSS, CSS3",
+    skill: 'CSS, CSS3',
     description: `
       Styling with CSS and CSS3.</br>
       <strong>CSS</strong>: Styles expected in a website (colors, fonts, ...).</br>
       <strong>CSS3</strong>: Composition of layouts with Grids and Flex-box, animations and transitions.
-      `
+      `,
   },
   {
-    skill: "SCSS",
+    skill: 'SCSS',
     description: `
       CSS extension</br>
       Sass modules, mixins, functions, nesting, ...
-      `
+      `,
   },
   {
-    skill: "Javascript",
+    skill: 'Javascript',
     description: `
     Frontend and Backend (Node.js)</br>
     <strong>General language knowledge</strong>: Data structures, control flow statements, DOM modifications, syntax, ...</br>
     <strong>Other</strong>: Clean code, basic design patterns knowledge, performance awareness.
-  `
+  `,
   },
   {
-    skill: "Typescript",
+    skill: 'Typescript',
     description: `
     Same experience as in Javascript but also applied to specific frameworks like <strong>Angular</strong> and <strong>React</strong>.
-  `
+  `,
   },
   {
-    skill: "Angular",
+    skill: 'Angular',
     description:
-      "Angular basics, styling, routing, services, RxJs Observables NgRx state management"
+      'Angular basics, styling, routing, services, RxJs Observables NgRx state management',
   },
   {
-    skill: "Angular Material",
-    description: "Experience with material components and components API"
+    skill: 'Angular Material',
+    description: 'Experience with material components and components API',
   },
   {
-    skill: "React",
+    skill: 'React',
     description:
-      "React basics, state management (Redux and Library-specific), React Hooks, Routing, class and function components, access to http content, React typescript, styling."
+      'React basics, state management (Redux and Library-specific), React Hooks, Routing, class and function components, access to http content, React typescript, styling.',
   },
   {
-    skill: "React bootstrap",
-    description: "Basic styling knowledge using React Boostrap"
+    skill: 'React bootstrap',
+    description: 'Basic styling knowledge using React Boostrap',
   },
   {
-    skill: "JQuery",
-    description: "JQuery basics, DOM manipulation, AJAX, HTML events"
+    skill: 'JQuery',
+    description: 'JQuery basics, DOM manipulation, AJAX, HTML events',
   },
   {
-    skill: "Electronjs",
+    skill: 'Electronjs',
     description:
-      "Building applications for Windows. IpcMain and IpcRender comunication"
+      'Building applications for Windows. IpcMain and IpcRender comunication',
   },
   {
-    skill: "Bootstrap 4",
-    description: "Basic styling knowledge using Boostrap 4"
+    skill: 'Bootstrap 4',
+    description: 'Basic styling knowledge using Boostrap 4',
   },
   {
-    skill: "Ionic 4 + Capacitor",
+    skill: 'Ionic 4 + Capacitor',
     description:
-      "Basic framework knowledge. Capacitor as tool to build mobile applications."
+      'Basic framework knowledge. Capacitor as tool to build mobile applications.',
   },
-  { skill: "Git", description: "" },
-  { skill: "Github", description: "" },
-  { skill: "Flutter", description: "" },
   {
-    skill: "Jira",
-    description: "Jira is a issue tracking and project management tool."
-  }
+    skill: 'Express.js',
+    description: 'Library used to make REST API.',
+  },
+  {
+    skill: 'Amazon Web Services',
+    description:
+      'Basic knowledge of AWS API Gateway, AWS Lambdas, DynamoDB, AppSync (Graphql)',
+  },
+  { skill: 'Git', description: '' },
+  { skill: 'Github', description: '' },
+  { skill: 'Flutter', description: '' },
+  {
+    skill: 'Jira',
+    description: 'Jira is a issue tracking and project management tool.',
+  },
 ];
 
 @Component({
-  selector: "skills-details",
-  templateUrl: "./skills-details.component.html",
-  styleUrls: ["./skills-details.component.scss"]
+  selector: 'skills-details',
+  templateUrl: './skills-details.component.html',
+  styleUrls: ['./skills-details.component.scss'],
 })
 export class SkillsDetailsComponent {
-  displayedColumns: string[] = ["skill", "description"];
+  displayedColumns: string[] = ['skill', 'description'];
   dataSource = SKILL_DATA;
 }
