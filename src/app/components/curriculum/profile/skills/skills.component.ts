2839b73dec5a517656ac65982e681395b6012712
juaamol <jambulud@gmail.com>
2023-02-17 20:59:06 +0100

    fix delayedProgress

--- src/app/components/curriculum/profile/skills/skills.component.ts
+++ src/app/components/curriculum/profile/skills/skills.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  AfterViewInit,
+  AfterContentInit,
+} from '@angular/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss'],
 })
-export class SkillsComponent implements OnInit, AfterViewInit {
+export class SkillsComponent implements OnInit {
   @Input() value: string | number = 0;
   mode: ProgressBarMode = 'determinate';
   delayedProgress: string | number = 0;
 
   constructor() {}
 
-  ngOnInit() {}
-
-  ngAfterViewInit(): void {
-    this.delayedProgress = this.value;
+  ngOnInit() {
+    of(this.value)
+      .pipe(delay(0))
+      .subscribe((value) => {
+        this.delayedProgress = value;
+      });
   }
 }
